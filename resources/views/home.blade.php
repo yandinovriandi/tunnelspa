663eb305d71a68b151b83897aed356a97f25fc12
yandi <yandi@yandis-iMac.local>
2023-04-01 03:25:03 +0700

    wip

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,5 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('TunnelMikrotikBot') }}
-        </h2>
-    </x-slot>
-    <!-- Start block -->
+ <x-home-layout>
+    <x-navbar/>
     <section class="bg-white dark:bg-gray-900">
         <div class="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
             <div class="mr-auto place-self-center lg:col-span-7">
@@ -205,4 +200,4 @@
             </div>
         </div>
     </footer>
-</x-app-layout>
+</x-home-layout>
---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,10 @@
+<header class="fixed w-full">
+    <nav class="bg-white border-gray-200 py-2.5 dark:bg-gray-900">
+        <div class="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
+            <Link href="{{route('home')}}" class="flex items-center">
+            <img src="{{asset('landwind/images/logo.svg')}}" class="h-6 mr-3 sm:h-9" alt="Tunnel Logo" />
+            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-gray-800">TunnelMikrotikBot</span>
+            </Link>
+        </div>
+    </nav>
+</header>
