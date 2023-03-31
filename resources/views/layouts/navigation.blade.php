cb79b8b7e8c16248f0d5a5fc6737649af1ea2a5f
yandi <yandi@yandis-iMac.local>
2023-03-31 12:23:40 +0700

    wip

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -70,6 +70,9 @@
                             <x-dropdown-link :href="route('dashboard')">
                                 {{ __('Dashboard') }}
                             </x-dropdown-link>
+                            <x-dropdown-link :href="route('transaction.create')">
+                                {{ __('Balance') }}
+                            </x-dropdown-link>
                             @can('create payment')
                             @php
                                 $payment = \App\Models\Payment::get();
@@ -134,6 +137,9 @@
             <div class="pt-2 pb-3 space-y-1">
                 <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                     {{ __('Dashboard') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('transaction.create')" :active="request()->routeIs('transaction.*')">
+                    {{ __('Saldo') }}
                 </x-responsive-nav-link>
                 <x-responsive-nav-link :href="route('server.index')" :active="request()->routeIs('server*')">
                     {{ __('Servers') }}
